1403dbedd6727ed23947fb483ca3bfcb16b415d7
neizer1 <116435195+neizer1@users.noreply.github.com>
2023-08-31 16:51:57 +0000

    Add files via upload

---
+++ VI_Fashion_Ent/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contactus.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == '__main__':
+    app.run()
